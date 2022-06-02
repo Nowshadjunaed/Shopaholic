535cec1ef7d48af630b8f8a74464fbde81bd3f01
Jahedul Alam Junaed <nowshadjunaed@gmail.com>
2022-06-03 03:39:16 +0600

    add initial frontend structure

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import { Container } from 'react-bootstrap';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+const App = () => {
+  return (
+    <>
+    
+    <Header />
+      <main className='py-3'>
+
+        <Container>
+          <h1>Welcome To Ecommerce</h1>
+        </Container>
+
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
