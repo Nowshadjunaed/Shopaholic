a3bf261a4fd89a7506004d7dc7b20105d23ea6cf
Omar Faruqe <riyad.omf@gmail.com>
2022-06-05 03:46:41 +0600

    Add Product routes and Product pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,26 @@
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <>
-    
-    <Header />
+    <Router>
+      <Header />
       <main className='py-3'>
 
         <Container>
-          <HomeScreen />
+          <Routes>
+          <Route exact path='/' element={<HomeScreen />} />
+          <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
 
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
